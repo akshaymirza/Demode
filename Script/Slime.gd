21f4c247f0cf20be41baadb2dd6abd3f1eb7fc71
shaliza <akshayalizar12@gmail.com>
2026-01-20 14:05:15 +0700

    golem

--- Script/Slime.gd
+++ Script/Slime.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 const SPEED = 60
 var direction = 1
-var health = 1
+var health = 10
 
 @onready var ray_cast_right = $RayCastright
 @onready var ray_cast_left = $RayCastleft
@@ -26,8 +26,6 @@ func _on_area_2d_body_entered(body):
 	# Kita asumsikan pemain memiliki variabel 'health'
 	if body.name == "Player" or body.has_method("die") or "health" in body:
 		body.health -= 10
-		print("-10") # Mencetak angka 10 (atau -10) ke konsol
-		
 		# Jika kamu ingin memastikan fungsi die() dipanggil saat darah 0
 		if body.health <= 0 and body.has_method("die"):
 			body.die()
